ef6ed32369dc71813f1a13a23f248d8f4444721d
konglingna <kln54321@163.com>
2022-05-25 22:44:28 +0800

    SpringBoot 依赖管理、自动配置 @Configuration注解

---
+++ src/main/java/com/cfht/boot/config/MyConfig.java
@@ -0,0 +1,35 @@
+package com.cfht.boot.config;
+
+import com.cfht.boot.bean.Pet;
+import com.cfht.boot.bean.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 1、配置类里面使用@Bean标注在方法上给容器注册组件，默认也是单实例的
+ * 2、配置类本身也是组件
+ * 3、proxyBeanMethods:代理bean的方法
+ *     Full(proxyBeanMethods = true)
+ *     Lite(proxyBeanMethods = false)
+ *     组件依赖
+ */
+//@Configuration(proxyBeanMethods = true)
+@Configuration  //告诉SpringBoot这是一个配置类 == 配置文件
+public class MyConfig {
+    /**
+     * 外部无论对配置类中的这个组件注册方法调用多少次获取的都是之前注册容器中的单实例对象
+     * @return
+     */
+    @Bean//给容器中添加组件。以方法名作为组件id。返回类型就是组件类型。返回的值，就是组件在容器中的实例
+    public User user01(){
+        User zhangsan = new User("zhangsan", 18);
+        //user组件依赖Pet组件
+        zhangsan.setPet(tomcatPet());
+        return zhangsan;
+    }
+
+    @Bean("tom")//自定义组件名
+    public Pet tomcatPet(){
+        return new Pet("tomcat");
+    }
+}
